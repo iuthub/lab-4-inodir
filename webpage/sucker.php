868a430992964ad6810ae50ee54dae93ec963dec
inodir <nodirutkirov84@gmail.com>
2018-03-07 22:59:11 +0500

    Ex3

--- webpage/sucker.php
+++ webpage/sucker.php
@@ -11,9 +11,17 @@
 
 		<?php
 			 	$name=$_REQUEST["name"];
-			  	$section =$_REQUEST["section"];
-			    $Creditnumber=$_REQUEST["Creditnumber"];
-			    $Credittype=$_REQUEST["CreditCard"];
+			  	 $section =$_REQUEST["section"];
+			      $Creditnumber=$_REQUEST["Creditnumber"];
+			       $Credittype=$_REQUEST["CreditCard"];
+			        $myfile=fopen("sucker.txt", "a") or die("Unable to open file!");
+
+			        		fwrite($myfile, $name. ";");
+			        		fwrite($myfile, $section. ";");
+			        		fwrite($myfile, $Creditnumber. ";");
+			        		fwrite($myfile, $Credittype. ",");
+			        		fwrite($myfile, PHP_EOL);
+			        		fclose($myfile);
 			?>
 
 			<p>Your information has been recorded</p>
@@ -28,5 +36,15 @@
 				<dt>Credit Card</dt>
 				<dd><?=$Creditnumber ?>  (<?=$Credittype ?>)</dd>
 			</dl>
+
+			<p>here are all the suckers who submitted here</p>
+				
+				<?php
+						foreach(file("sucker.txt") as $line){
+						?>
+						<pre><?=$line ?></pre>
+						<?php
+					}
+					?>
 	</body>
 	</html>
